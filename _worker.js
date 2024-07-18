b71c40704bba3f13cc0145cc8e89c5cce663b13d
wuzhenping1 <51026882+wuzhenping1@users.noreply.github.com>
2024-07-18 15:56:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = ea077b7d-61fb-40bf-aef2-97716a53bb59';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
